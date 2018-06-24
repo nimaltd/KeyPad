115752d38095ae81954dabbf7daf57cdb336c2c7
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2018-06-24 10:01:04 +0430

    Add files via upload

--- KeyPad.c
+++ KeyPad.c
@@ -33,12 +33,12 @@ uint16_t	KeyPad_Scan(void)
 			if(HAL_GPIO_ReadPin((GPIO_TypeDef*)_KEYPAD_ROW_GPIO_PORT[r],_KEYPAD_ROW_GPIO_PIN[r])==GPIO_PIN_RESET)
 			{
 				_KEYPAD_DELAY(_KEYPAD_DEBOUNCE_TIME_MS);
-				while(HAL_GPIO_ReadPin((GPIO_TypeDef*)_KEYPAD_ROW_GPIO_PORT[r],_KEYPAD_ROW_GPIO_PIN[r])==GPIO_PIN_RESET)
-					_KEYPAD_DELAY(5);
 				if(HAL_GPIO_ReadPin((GPIO_TypeDef*)_KEYPAD_ROW_GPIO_PORT[r],_KEYPAD_ROW_GPIO_PIN[r])==GPIO_PIN_RESET)
 				{
 					key |= 1<<c;					
 					key |= 1<<(r+8);
+					while(HAL_GPIO_ReadPin((GPIO_TypeDef*)_KEYPAD_ROW_GPIO_PORT[r],_KEYPAD_ROW_GPIO_PIN[r])==GPIO_PIN_RESET)
+						_KEYPAD_DELAY(5);
 					return key;
 				}
 			}			
